22cf8e241bba33f025b32f5db28e9e7f83b38a9a
orloveste <orlovevm@yahoo.com>
2020-12-06 10:37:26 +0200

    arunca exceptie la metoda - testeaza exceptia metodei

--- src/test/java/service/BusinessServiceTest.java
+++ src/test/java/service/BusinessServiceTest.java
@@ -20,6 +20,21 @@ public class BusinessServiceTest {
         //foloseste framework de Assertions - multiple optiuni de testare
 
     }
+    @Test
+    public void testCalculeazaTaxeSalariuException() {
+//1 crearea contextului de testare
+        BusinessService service = new BusinessService();
+        int salariu = -100;//definesti si datele de intrare
+        int percent = 15;
 
+        //2. cand? invocarea metodei de test folosit in contextul de la pasul 1
+        double result = service.calculeazaTaxeSalariu(salariu, percent);
+        // sa invoc un obiect si sa-i dau parametri - important testezi o metoda intr-o clasa
+
+        //3. validare rezultat
+        Assertions.assertEquals(15, result);// validezi rezultatul
+        //foloseste framework de Assertions - multiple optiuni de testare
+
+    }
 
 }
