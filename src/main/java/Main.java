c1ce4f97d29759b83e503f55897fe82560ebed60
orloveste <orlovevm@yahoo.com>
2020-12-06 10:17:19 +0200

    main ruleaza datele - versiune lizibila mai clara

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ import service.BusinessService;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new BusinessService().calculeazaTaxeSalariu(100, 15));
+        BusinessService service = new BusinessService();
+        double tax = service.calculeazaTaxeSalariu(100,15);
+        System.out.println(tax);
+//        System.out.println(new BusinessService().calculeazaTaxeSalariu(100, 15));
     }
 }
