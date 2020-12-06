4b3b3163073275e46ee53d460cab7851cbac0f09
orloveste <orlovevm@yahoo.com>
2020-12-06 11:48:56 +0200

    testeaza deduceri -

--- src/main/java/service/BusinessService.java
+++ src/main/java/service/BusinessService.java
@@ -7,4 +7,12 @@ public class BusinessService {
         }
         return (((double) salariu) * percent) / 100;
     }
+    public int calculeazaDeduceri(int salariu, int persoane) {
+        if (persoane == 1) return 0;
+        if (persoane == 2 && salariu < 1000) return 10;
+        if (persoane == 3 && salariu < 1000) return 15;
+        if (persoane == 4 && salariu < 1000) return 20;
+        if (persoane >= 5 && salariu < 1000) return 30;
+        return 0;
+    }
 }
--- src/test/java/service/BusinessServiceTest.java
+++ src/test/java/service/BusinessServiceTest.java
@@ -48,4 +48,24 @@ public class BusinessServiceTest {
         // validare rezultat
 //        Assertions.assertEquals(15, result);
     }
+
+    @Test
+    public void testCalculeazaDeduceri() {
+        //1
+
+        //2
+        int result1 = service.calculeazaDeduceri(999, 1);
+        int result2 = service.calculeazaDeduceri(999, 2);
+        int result3 = service.calculeazaDeduceri(999, 3);
+        int result4 = service.calculeazaDeduceri(999, 4);
+        int result5 = service.calculeazaDeduceri(999, 5);
+        int result6 = service.calculeazaDeduceri(1000, 5);
+        //3
+        Assertions.assertEquals(0, result1);
+        Assertions.assertEquals(10, result2);
+        Assertions.assertEquals(15, result3);
+        Assertions.assertEquals(20, result4);
+        Assertions.assertEquals(30, result5);
+        Assertions.assertEquals(0, result6);
+    }
 }
