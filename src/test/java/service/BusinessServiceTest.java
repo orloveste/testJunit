a4a443d0e88339fc470e433df41078c58cbb3dea
orloveste <orlovevm@yahoo.com>
2020-12-06 10:56:37 +0200

    doar metodele publice se testeaza, tot ce nu este expus in exterior = subject to change. - Junit = before, after - beforeEach - utilizeaza

--- src/test/java/service/BusinessServiceTest.java
+++ src/test/java/service/BusinessServiceTest.java
@@ -1,31 +1,38 @@
 package service;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BusinessServiceTest {
+    BusinessService service;
+    int salariu;
+    int percent;
+
+    @BeforeEach
+    public void setUp() {
+        service = new BusinessService();
+        int salariu = 100;//definesti si datele de intrare
+        int percent = 15;
+    }
     @Test
     public void testCalculeazaTaxeSalariu() {
 //1 crearea contextului de testare
-        BusinessService service = new BusinessService();
-        int salariu = 100;//definesti si datele de intrare
-        int percent = 15;
+
 
         //2. cand? invocarea metodei de test folosit in contextul de la pasul 1
         double result = service.calculeazaTaxeSalariu(salariu, percent);
         // sa invoc un obiect si sa-i dau parametri - important testezi o metoda intr-o clasa
 
         //3. validare rezultat
-        Assertions.assertEquals(14, result);// validezi rezultatul
+        Assertions.assertEquals(15, result);// validezi rezultatul
         //foloseste framework de Assertions - multiple optiuni de testare
 
     }
     @Test
     public void testCalculeazaTaxeSalariuException() {
 //1 crearea contextului de testare
-        BusinessService service = new BusinessService();
-        int salariu = -100;//definesti si datele de intrare
-        int percent = 15;
+        salariu = -100;
 
         //2. cand? invocarea metodei de test folosit in contextul de la pasul 1
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
