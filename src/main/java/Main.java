85655d40a5944760c19ad8cdefe4f24eea0f03fe
orloveste <orlovevm@yahoo.com>
2020-12-06 10:18:49 +0200

    main ruleaza datele - exceptia main individuala

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,9 @@ import service.BusinessService;
 public class Main {
     public static void main(String[] args) {
         BusinessService service = new BusinessService();
-        double tax = service.calculeazaTaxeSalariu(100,15);
+        double tax = service.calculeazaTaxeSalariu(100,16);
+        if (tax != 15) throw  new RuntimeException("eroare de calcul");
+
         System.out.println(tax);
 //        System.out.println(new BusinessService().calculeazaTaxeSalariu(100, 15));
     }
