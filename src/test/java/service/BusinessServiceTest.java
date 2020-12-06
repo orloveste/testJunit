bb4e7911e2dc10ec37a57e81b74fc8a8db06b83d
orloveste <orlovevm@yahoo.com>
2020-12-06 10:43:16 +0200

    arunca exceptie la metoda - testeaza exceptia metodei - ca sa o treaca cu jUinit

--- src/test/java/service/BusinessServiceTest.java
+++ src/test/java/service/BusinessServiceTest.java
@@ -28,11 +28,14 @@ public class BusinessServiceTest {
         int percent = 15;
 
         //2. cand? invocarea metodei de test folosit in contextul de la pasul 1
-        double result = service.calculeazaTaxeSalariu(salariu, percent);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            service.calculeazaTaxeSalariu(salariu, percent);
+        });
+
         // sa invoc un obiect si sa-i dau parametri - important testezi o metoda intr-o clasa
 
         //3. validare rezultat
-        Assertions.assertEquals(15, result);// validezi rezultatul
+
         //foloseste framework de Assertions - multiple optiuni de testare
 
     }
