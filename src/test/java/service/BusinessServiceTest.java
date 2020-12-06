b798a06f9a607f03341b5c1bc573ff6c0273c989
orloveste <orlovevm@yahoo.com>
2020-12-06 10:23:01 +0200

    Testele sa fie in oglinda ca organizare - testarea metodelor si baterii de testare.

---
+++ src/test/java/service/BusinessServiceTest.java
@@ -0,0 +1,14 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+public class BusinessServiceTest {
+    @Test
+    public void testIt() {
+
+    }
+    @Test
+    public void testIt1() {
+
+    }
+}
