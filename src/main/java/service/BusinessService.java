8ab90d41fd8b779883e549cff52d5f46e7c1dca9
orloveste <orlovevm@yahoo.com>
2020-12-06 10:34:39 +0200

    arunca exceptie la metoda

--- src/main/java/service/BusinessService.java
+++ src/main/java/service/BusinessService.java
@@ -2,6 +2,9 @@ package service;
 
 public class BusinessService {
     public double calculeazaTaxeSalariu(int salariu, int percent) {
+        if(salariu <= 0) {
+            throw new IllegalArgumentException("salariu negativ");
+        }
         return (((double) salariu) * percent) / 100;
     }
 }
